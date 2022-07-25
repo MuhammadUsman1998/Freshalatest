f3094656d46e78f57004f956aff11b410d0b8f4b
Usman Ajmal <usman.ajmal@appsgenii.eu>
2022-07-25 22:36:24 +0500

    add server_IP localhost:5000

--- src/config.js/env.jsx
+++ src/config.js/env.jsx
@@ -1,2 +1,5 @@
 // export const SERVER_IP = "http://192.168.1.89:5000";
-export const SERVER_IP = "https://fresha-backend.herokuapp.com";
+// export const SERVER_IP = "https://fresha-backend.herokuapp.com";
+
+
+export const SERVER_IP = "http://localhost:5000";
